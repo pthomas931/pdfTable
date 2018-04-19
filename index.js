ddb5b95c33e2e200a687d19270183f5acd0bb791
Phillip <pthomas931@gmail.com>
2018-04-18 22:40:56 -0400

    Improved parsing

--- index.js
+++ index.js
@@ -17,24 +17,23 @@ PDFJS.getDocument('test.pdf').then(function(pdf) {
   pdf.getPage(2).then(function(page) {
     page.getTextContent({disableCombineTextItems: true}).then(function(txt) {
       let chunks = [];
+      let chunk = [];
       let prevSpot = 0;
 
       txt.items.forEach((item) => {
         let spot = item.transform[item.transform.length - 1];
 
         if(!excludeArray.includes(item.str)) {
-          let chunk = [];
-          item.str.split('   ').forEach(key => chunk.push(key));
+          let chunkStr = [];
+          item.str.split('   ').forEach(key => chunkStr.push(key));
 
-          console.log(item, chunk);
-          if ((prevSpot - spot) > 20 || prevSpot == 0) {
-            console.log('new chunk');
+          if ((prevSpot - spot) > 20 && prevSpot != 0) {
             // Assume new line
             chunks.push(chunk);
-          } else {
-            console.log('append chunk');
-            chunks[chunks.length - 1] += ` ${chunk}`;
+            chunk = [];
           }
+
+          chunk.push(chunkStr);
         }
         prevSpot = spot;
       });
