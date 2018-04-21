a59fc59a97076c37764b302390ce3ca39b91ea04
Phillip <pthomas931@gmail.com>
2018-04-20 21:23:34 -0400

    Removed dead code

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-/*let fs = require('fs'),
-  PDFParser = require("pdf2json");
-
-let pdfParser = new PDFParser();
-
-pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError) );
-pdfParser.on("pdfParser_dataReady", pdfData => {
-    console.log(JSON.stringify(pdfData));
-});
-
-pdfParser.loadPDF("./test.pdf");*/
-
 let excludeArray = ['No messages found in current search.'];
 
 let PDFJS = require('pdfjs-dist');
